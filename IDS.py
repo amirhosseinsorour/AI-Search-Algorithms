fe2705b9f4ccf88fd0e60ba9bf85b09f416553de
Amirhossein <46843953+amirhosseinsorour@users.noreply.github.com>
2021-08-21 15:37:22 +0430

    Algorithms added

---
+++ IDS.py
@@ -0,0 +1,140 @@
+import copy
+
+
+class Node(object):
+    def __init__(self):
+        self.environments = []
+        self.children = []
+        self.parent = None
+        self.goal_way = None
+        self.process = ""
+        self.visited = False
+        self.depth = 0
+
+    def add_environment(self, environment):
+        self.environments.append(environment)
+
+    def add_environments(self, environments):
+        self.environments = copy.deepcopy(environments)
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+    def add_parent(self, obj):
+        self.parent = obj
+
+    def define_process(self, process):
+        self.process += process
+
+    def set_depth(self, root):
+        if self != root and self.depth == 0:
+            self.depth = self.parent.depth + 1
+
+    def is_goal(self):
+        for e in self.environments:
+            global n
+            if string_to_card(e[0])[0] != n and string_to_card(e[0])[1] != "#":
+                return False
+            for i in range(len(e) - 1):
+                if string_to_card(e[i])[0] != string_to_card(e[i + 1])[0] + 1:
+                    return False
+                if string_to_card(e[i])[1] != string_to_card(e[i + 1])[1]:
+                    return False
+
+        return True
+
+    def expand(self):
+        if self.visited:
+            return
+        for i, e1 in enumerate(self.environments):
+            card1 = e1[len(e1) - 1]
+            num1, color1 = string_to_card(card1)
+            for j, e2 in enumerate(self.environments):
+                if i == j:
+                    continue
+                card2 = e2[len(e2) - 1]
+                num2, color2 = string_to_card(card2)
+                if num2 > num1:
+                    global nodes_count
+                    nodes_count += 1
+                    newChild = Node()
+                    newChild.add_environments(self.environments)
+                    card = newChild.environments[i].pop()
+                    if len(newChild.environments[i]) == 0:
+                        newChild.environments[i].append("#")
+                    if color2 == "#":
+                        newChild.environments[j].pop()
+                    newChild.environments[j].append(card)
+                    newChild.define_process("from column %d to column %d" % (i + 1, j + 1))
+                    newChild.add_parent(self)
+                    self.add_child(newChild)
+        self.visited = True
+
+
+def string_to_card(str):
+    l = len(str) - 1
+    if l > 0:
+        number = int(str[:l])
+        color = str[l]
+        return number, color
+    if l == 0:
+        global n
+        return n + 1, "#"
+
+
+def define_way(root, goal):
+    count = 0
+    while goal != root:
+        p = goal.parent
+        p.goal_way = goal
+        goal = p
+        count += 1
+    return count
+
+
+def print_way(root, depth):
+    node = root
+    for i in range(depth + 1):
+        if i > 0:
+            print(node.process)
+        print(node.environments, end="\n\n")
+        node = node.goal_way
+
+
+def IDS(root, limit):
+    stack = [root]
+    while len(stack) != 0:
+        node = stack.pop()
+        if node.is_goal():
+            return node
+        node.set_depth(root)
+        if node.depth == limit:
+            continue
+        node.expand()
+        global visited_nodes_count
+        visited_nodes_count += 1
+        for child in node.children:
+            stack.append(child)
+    goal = IDS(root, limit + 1)
+    return goal
+
+
+def main():
+    global k, m, n
+    k, m, n = [int(x) for x in input().split()]
+    root = Node()
+    for i in range(k):
+        root.add_environment(input().split())
+    global nodes_count, visited_nodes_count
+    nodes_count = visited_nodes_count = 0
+    limit = 0
+    goal = IDS(root, limit)
+    depth = define_way(root, goal)
+    print("N = %d" % depth)
+    print_way(root, depth)
+    print("Number of Generated Nodes : %d" % (nodes_count + 1))
+    print("Number of Visited nodes : %d" % visited_nodes_count)
+
+
+if __name__ == '__main__':
+    main()
